c3ee7aee15dd4b4ead4d00bee80e898efcfa88ee
dantedaemon <pwhelper@yandex.ru>
2023-01-25 02:32:42 +0500

    FixTypo

--- frontend/src/app/components/library-overview/library-overview.component.ts
+++ frontend/src/app/components/library-overview/library-overview.component.ts
@@ -95,7 +95,6 @@ export class LibraryOverviewComponent implements AfterContentInit {
           this.availablePlatforms = result[3];
 
           this.previousStateParams = this.route.snapshot.queryParams;
-          if (this.previousStateParams['showTitles'] !== undefined) this.alwaysShowTitles = this.previousStateParams['showTitles'];
           if (this.previousStateParams['search'] !== undefined) this.searchTerm = this.previousStateParams['search'];
           if (this.previousStateParams['sort'] !== undefined) this.selectedSortOption = this.matchSelectedSortOptionFromParam(this.previousStateParams['sort']);
           if (this.previousStateParams['gamemodes'] !== undefined) this.setSelectedGamemodesFromParam(this.previousStateParams['gamemodes']);
